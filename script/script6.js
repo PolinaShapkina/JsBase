a58400cee2e1515023b754698d9f29baf608fd47
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-04 23:03:12 +0300

    Task6

---
+++ script/script6.js
@@ -0,0 +1,21 @@
+let Smeshariki = 632;
+alert(Smeshariki);
+// number 2
+let a = 2;
+let b = 4;
+let c = 6;
+
+let d = 2, f = 4, e = 6;
+
+let aa, bb, cc;
+aa = 2;
+bb = 4;
+cc = 6;
+// number 3
+let smeshariki;  
+
+smeshariki= 10;    
+alert(smeshariki); 
+
+smeshariki = 20;     
+alert(smeshariki); 
