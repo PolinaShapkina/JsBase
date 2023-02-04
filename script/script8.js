2fac2c2a029633766c83eba43c7e89831973c685
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-04 23:09:00 +0300

    Task8

---
+++ script/script8.js
@@ -0,0 +1,19 @@
+//1
+let a = 10, b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+//  2
+let a1 = 10, b1 = 5;
+result = a1 + b1;
+alert(result);
+//  3
+let a2 = 1, b2 = 2, c2 = 3;
+alert(a2 + b2 + c2);
+//  4
+let a3 = 10, b3 = 5;
+c = a3 - b3;
+let d = 7;
+result1 = c + d;
+alert(result1);
