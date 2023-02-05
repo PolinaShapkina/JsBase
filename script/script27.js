e3e541fb2a68659fb770275c4bf5256ba624bba6
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 21:06:15 +0300

    Task27

---
+++ script/script27.js
@@ -0,0 +1,9 @@
+let str = 'abcde';
+alert(str[0]); 
+alert(str[2]); 
+alert(str[4]); 
+let str1 = 'abcde';
+alert(str1[4]+str1[3]+str1[2]+str1[1]+str1[0]); 
+let str2 = 'abcde';
+let num = 3; 
+alert(str2[num]);
