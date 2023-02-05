1d9c2ef541c14b41b6a7bd8aa2f76f2215dd093f
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 16:17:23 +0300

    Task23

---
+++ script/script23.js
@@ -0,0 +1,6 @@
+let a = parseInt('5px'), b = parseInt('6px');
+console.log(a+b);
+let c = parseFloat('5.5px'), d = parseFloat('6.25px');
+console.log(c+d);
+let v = parseFloat('5.5px'), g = parseFloat('6.25px');
+console.log(v+g+'px');
