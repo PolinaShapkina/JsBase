79e70911dab50913c839625a4f693c9c837944e3
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 15:50:33 +0300

    Task18

---
+++ script/script18.js
@@ -0,0 +1,19 @@
+// 1
+let a;
+alert(a);
+// 2
+let b = null;
+alert(b);
+// 3
+let v = true;
+alert(v);
+// 4
+let d = false;
+alert(d);
+// 5
+let p = 'Polina', s = 'Shapkina';
+alert(p * s);
+// 6
+alert( 10 / 0);
+// 7 
+alert(-10 / 0); 
