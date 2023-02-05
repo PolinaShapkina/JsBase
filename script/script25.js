ac1e39578dca685c448aadb33385f022e9bdf836
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 16:30:52 +0300

    Task25

---
+++ script/script25.js
@@ -0,0 +1,7 @@
+console.log(true + 3);
+console.log(true + true);
+console.log(true - true);
+console.log(true + false);
+console.log(true + '1');
+console.log(String(true) + 1);
+console.log(String(true) + Number(true));
