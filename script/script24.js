3c58caf9d76d99f7a9f179e5b543382e96c47f9d
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-05 16:28:00 +0300

    Task24

---
+++ script/script24.js
@@ -0,0 +1,6 @@
+let a = '123', b = '456';
+console.log(String(a)+String(b));
+let v = '123';
+console.log(String(v).length);
+let g = '123', d = '456';
+console.log(String(g).length+String(d).length);
