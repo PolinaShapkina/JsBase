8e000f30229edc9e25d71b1fafe8c33be2249236
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-13 19:37:22 +0300

    Task31

---
+++ script/script31.js
@@ -0,0 +1,21 @@
+let num = 10;
+num++;
+num++;
+num--;
+alert(num);
+// 2
+let num1 = 3;
+alert(++num1);
+// 3
+let num2 = 3;
+alert(num2++);
+// 4
+let num3 = 3;
+let num4 = ++num3;
+alert(num3);
+alert(num4);
+// 5
+let num5 = 3;
+let num6 = --num5;
+alert(num5);
+alert(num6)
