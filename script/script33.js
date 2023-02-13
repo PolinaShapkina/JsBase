4d7c7dc13717fec7d10e3dc4a799e6e1a202dc0f
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-13 23:16:54 +0300

    Task33

---
+++ script/script33.js
@@ -0,0 +1,12 @@
+alert('Ваш возраст: ' + prompt('Ваш возраст?')); 
+// 2
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+alert(Number(num1) + Number(num2)); 
+// 3
+let sguare = prompt('Сторона квадрата = ');
+alert(sguare*sguare);
+// 4
+let num3 = prompt('Введите первое число');
+let num4 = prompt('Введите второе число');
+alert(Number(num3) + Number(num4));
