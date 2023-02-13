cadacc2306781d21f87ebdd090f44bb1dfa1bbfd
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-13 18:39:58 +0300

    Task29

---
+++ script/script29.js
@@ -0,0 +1,11 @@
+let aaa = '12345'; 
+console.log(Number(aaa[0]) + Number(aaa[1])+Number(aaa[2]) + Number(aaa[3])+Number(aaa[4]))
+// 2
+let bbb = String(12345); 
+console.log(Number(bbb[0]) + Number(bbb[1])+ Number(bbb[2]) + Number(bbb[3])+ Number(bbb[4]))
+// 3
+let ccc = String(12345); 
+console.log(ccc[0] * ccc[1] * ccc[2] * ccc[3]* ccc[4]);
+// 4
+let ddd = '12345'; 
+console.log(ddd[4] + ddd[3] + ddd[2] + ddd[1] + ddd[0]);
