22c525a11510704fc0bba30e253313175a08a43c
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-13 23:23:34 +0300

    Task34

---
+++ script/script34.js
@@ -0,0 +1,9 @@
+document.write('Polina');
+document.write('<p style="font-style: italic;">Polina</p>');
+let str = 'text';
+document.write('<p style="font-style: italic;">Shapkina</p>');
+document.write('1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5<br>');
