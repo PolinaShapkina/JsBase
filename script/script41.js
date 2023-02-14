5cd20093caee79221a4c833ddcb9fce223f04ec8
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-14 20:37:29 +0300

    Task41

---
+++ script/script41.js
@@ -0,0 +1,15 @@
+let arr1 = [1, 2, 3];
+arr1[0]++;
+arr1[1]++;
+arr1[2]++;
+console.log(arr1);
+let arr2 = [1, 2, 3, 4];
+arr2[0] += 3;
+arr2[1] += 3;
+arr2[2] += 3;
+arr2[3] += 3;
+console.log(arr2)
+let arr3 = ['a', 'b', 'c'];
+arr3[0] = 1;
+arr3[1] = 2;
+arr3[2] = 3;
