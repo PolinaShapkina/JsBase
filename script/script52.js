7fa02c3700ccd0abe6502c1750a749dbc43becc4
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-14 22:22:50 +0300

    Task52

---
+++ script/script52.js
@@ -0,0 +1,21 @@
+let obj = {
+	'1a': 1, 
+	'b2': 2,
+	'с-с': 3,
+	'd 4': 4,
+	'e5': 5
+};
+
+let obj1 = {
+	'1a': 1,
+	'b2': 2,
+	'с-с': 3,
+	'd 4': 4,
+	'e5': 5
+};
+
+console.log(obj1['1a']);
+console.log(obj1.b2);
+console.log(obj1['c-c']);
+console.log(obj1['d 4']);
+console.log(obj1.e5);
