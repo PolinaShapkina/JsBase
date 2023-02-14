6fb23d5566f60e0e6792e716e4e99f62b69b37f7
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-14 22:02:45 +0300

    Task47

---
+++ script/script47.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length-1]);
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1[0] + arr1[1] + arr1[2] + arr1[3] + arr1[4]);
+let arr2 = [1, 2, 3, 4, 5];
+console.log(arr2.length);
