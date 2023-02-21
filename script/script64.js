f42a0f4e9ab159ccfd2359a620af29d63791acc6
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-21 14:27:09 +0300

    Task64

---
+++ script/script64.js
@@ -0,0 +1,6 @@
+console.log( typeof {x: 1, y: 2, z: 3} ); 
+console.log( typeof {} );
+let obj = {x: 1, y: 2, z: 3};
+console.log( typeof obj );
+let obj1 = {x: 1, y: 2, z: 3};
+console.log( typeof obj1['x'] );
