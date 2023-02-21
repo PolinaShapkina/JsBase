eb64d5f31233daf29012cfeba51173293537214f
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-21 15:07:17 +0300

    Task79

---
+++ script/script79.js
@@ -0,0 +1,21 @@
+let test1 = '3';
+let test2 = '3';	
+if (test1 == test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test3 = 3;
+let test4 = '3';
+if (test3 === test4) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test5 = 3;
+let test6 = 3;	
+if (test5 === test6) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
