92d9a1fa89c50360cbd732a6464c82be6fa96e4c
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-21 14:31:02 +0300

    Task65

---
+++ script/zcript65.js
@@ -0,0 +1,8 @@
+console.log( typeof {x: 1, y: 2, z: 3} ); 
+console.log( typeof [1, 2, 3] );
+let arr = [1, 2, 3];
+console.log( typeof arr );
+let arr1 = [1, 2, 3];
+console.log( typeof arr[0] );
+let arr2 = ['1', '2', '3'];
+console.log( typeof arr[0] );
