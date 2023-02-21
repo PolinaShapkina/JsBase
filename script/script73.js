021c598b2ac76279ed9dbca111506754f5cff644
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-21 14:50:00 +0300

    Task73

---
+++ script/script73.js
@@ -0,0 +1,18 @@
+let test = 11;
+if (test > 10) {
+	console.log('Переменная больше 10');
+} else {
+	console.log('Переменная не больше 10');
+}
+let test1 = 9;
+if (test1 < 10) {
+	console.log('Переменная меньше 10');
+} else {
+	console.log('Переменная не меньше 10');
+}
+let test2 = 10;
+if (test2 >= 0) {
+	console.log('Переменная больше или равна 10');
+} else {
+	console.log('Переменная не больше или не равна 10');
+}
