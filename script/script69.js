517d0d9a4a2653f0a46dad190ca434685ad02cfa
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-21 14:40:33 +0300

    Task69

---
+++ script/script69.js
@@ -0,0 +1,9 @@
+const arr = ['a', 'b', 'c'];
+arr[1] = '!';
+console.log(arr);
+const arr1 = ['a', 'b', 'c'];
+arr1 = [1, 2, 3];
+console.log(arr1);
+const arr2 = ['a', 'b', 'c'];
+arr2 = ['a', 'b', 'c'];
+console.log(arr2);
