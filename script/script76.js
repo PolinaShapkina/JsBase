de04e2530f22323d6704c3cb06338763d04922ca
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-21 15:00:14 +0300

    Task76

---
+++ script/script76.js
@@ -0,0 +1,15 @@
+let test1 = 1;
+let test2 = 2;
+if (test2 > test1) {
+	console.log('test2 > test1');
+} else {
+	console.log('test2 < test1');
+}
+// 2
+let test3 = 1;
+let test4 = 2;
+if (test3 == test4) {
+	console.log('test3 == test4');
+} else {
+	console.log('test3 != test4');
+}
