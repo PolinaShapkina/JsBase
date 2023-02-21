eb6d57df3426c4e63db7e1a447186648814330c6
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-21 14:35:23 +0300

    Task67

---
+++ script/script67.js
@@ -0,0 +1,11 @@
+let test = {x: 1, y: 2, z: 3};
+console.log(test);
+let test1 = {x: 1, y: 2, z: 3};
+console.log(test1.x);
+let test2 = [1, 2, 3];
+console.log(test2);
+let test3 = [1, 2, 3];
+console.log(test3[1]);
+let test5 = [1, 2, 3];
+let test4 = 1;
+console.log(test5);
