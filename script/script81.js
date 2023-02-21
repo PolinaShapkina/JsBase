ca27812c8e09fee88ba798472d37b731bd12b7f7
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-21 15:11:38 +0300

    Task81

---
+++ script/script81.js
@@ -0,0 +1,19 @@
+let num = 3;
+if (num > 0 && num < 5) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let num1 = 11;
+if (num1 >= 10 && num1 <=20) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let num3 = -1;
+let num2 = 4;
+if (num2 >= 3 && num3 <=1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
