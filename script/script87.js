52bdfcd7ad0d89c461f971cfcf6bb49a3dbb8fdd
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-27 20:18:03 +0300

    Task87

---
+++ script/script87.js
@@ -0,0 +1,30 @@
+let test = 1;
+if (test == true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test1 = 0;
+if (test1 == true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test2 = 1;
+if (test2 != true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test3 = '';
+if (test3 == false) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test4;
+if (test4 == true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
