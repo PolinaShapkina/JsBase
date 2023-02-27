65413a83b6ec89dc3510f7f76c31fb027a8791ea
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-27 21:46:50 +0300

    Task94

---
+++ script/script94.js
@@ -0,0 +1,10 @@
+let day = 12;
+if (day <= 10){
+    console.log("1")
+} else if (day >= 11 && day <= 20){
+    console.log("2")
+} else if (day >= 21 && day<=31){
+    console.log("3")
+} else {
+    console.log("error")
+}
