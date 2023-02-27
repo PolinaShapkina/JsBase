a55204f6ee27db4e10b033478df7ccf72a2a5c92
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-27 20:14:39 +0300

    Task86

---
+++ script/script86.js
@@ -0,0 +1,12 @@
+let test = true;
+if (test === true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test1 = true;
+if (test1 === false) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
