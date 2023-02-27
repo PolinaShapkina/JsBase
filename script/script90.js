dfc05e5512963baf5003a33a10be4d7add279a52
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-02-27 20:25:59 +0300

    Task90

---
+++ script/script90.js
@@ -0,0 +1,30 @@
+let test = 3;
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test1 = 'abc';
+if (test1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test2 = '';
+if (test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test3 = null;
+if (test3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let test4 = -1;
+if (test4) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
