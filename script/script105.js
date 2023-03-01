6e87289bf9974edd4cde693f2d2014817c3bd235
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-01 20:37:28 +0300

    Task105

---
+++ script/script105.js
@@ -0,0 +1,13 @@
+let str = 'aytyt';
+if (str[0] == 'a') {
+	console.log('true');
+}
+let str1 = 'alkjhe';
+let last = str1[str1.length - 1];
+if (last == 'e') {
+	console.log('true');
+}
+let str2 = 'blkjg';
+if (str2[0] == 'a' || str2[0] == 'b') {
+	console.log('true');
+}
