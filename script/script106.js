341057cd199b7331c2a41364daa35510adc68895
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-01 20:39:54 +0300

    Task106

---
+++ script/script106.js
@@ -0,0 +1,13 @@
+let num = 123450;
+if (String(num)[5] == 0) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let n = 11;
+let last = String(n)[1];
+if (last == 0 || last == 2 || last == 4 || last == 6 || last == 8){
+    console.log('+++');
+} else {
+	console.log('---');
+}
