cd2f6429ef45b730a6fa827e662d6700a8880449
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-01 20:47:46 +0300

    Task109

---
+++ script/script109.js
@@ -0,0 +1,25 @@
+let month = 12;
+if (month == 1 || month == 2 || month == 12){
+    console.log("зима")
+} else if (month == 3 || month == 4 || month == 5){
+    console.log("весна")
+} else if (month == 6 || month == 7 || month == 8){
+    console.log("лето")
+} else if (month == 9 || month == 10 || month == 11){
+    console.log("осень")
+} else {
+    console.log("wrong number")
+}
+let str1 = 'abcde';
+if (num[0] == 'a'){
+    console.log('+++');
+} else {
+	console.log('---');
+}
+let n = 61324;
+let f = String(n)[0];
+if (f == 1 || f == 2 || f == 3){
+    console.log('+++');
+} else {
+	console.log('---');
+}
