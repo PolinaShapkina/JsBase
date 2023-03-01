7e3e2d736deeaaf169ec29ddf9516cb5e9ce5c0d
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-01 20:43:01 +0300

    Task107

---
+++ script/script107.js
@@ -0,0 +1,15 @@
+let a = 10;
+let rest = a % 2;
+if (rest === 0) {
+	console.log('делится нацело');
+} else {
+	console.log('делится с остатком' + rest); 
+}
+// 2
+let b = 20;
+let rest1 = b % 3;
+if (rest1 === 0) {
+	console.log('делится нацело');
+} else {
+	console.log('делится с остатком ' + rest1); 
+}
