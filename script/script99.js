d3c973d76e8c20b4a6f139d3ab88f568385a37b4
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-01 20:21:23 +0300

    Task99

---
+++ script/script99.js
@@ -0,0 +1,9 @@
+let a = 2 * (3 - 1);
+let b = 6 - 2;
+console.log(a == b)
+let a1 = 5 * (7 - 4);
+let b1 = 1 + 2 + 7;
+console.log(a1 == b1)
+let a2 = 2 ** 4;
+let b2 = 4 ** 2;
+console.log(a2 == b2)
