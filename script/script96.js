40bdf2a76ad6cb2fb989749b9b95a18af9a58b1d
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-01 20:13:10 +0300

    Task96

---
+++ script/script96.js
@@ -0,0 +1,16 @@
+let lang = 'ru';
+
+switch (lang) {
+    case 1 (lang == 'ru'):
+	console.log('рус');
+    break;
+    case 2 (lang == 'en'):
+	console.log('анг');
+    break;
+    case 3 (lang == 'de'):
+	console.log('нем');
+    break;
+    default:
+	console.log('язык не поддерживается');
+    break;
+}
