1b7900eec819d1289fda715b41d060b1d5a354c2
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-05 20:56:02 +0300

    Task115

---
+++ script/script115.js
@@ -0,0 +1,16 @@
+let arr = ['a', 'b', 'c', 'd', 'e'];
+for (let i = 0; i <= arr.length 
+	- 1; i++) { 
+	console.log(arr[i]);
+}
+console.log('2')
+let arr1 = ['a', 'b', 'c', 'd', 'e'];
+for (let i = 1; i <= arr1.length - 2; i++) { 
+	console.log(arr1[i]);
+}
+console.log('3')
+let arr2 = ['a', 'b', 'c', 'd', 'e'];
+for (let i = arr2.length - 1; i > 0; 
+	i--) { 
+	console.log(arr2[i]);
+}
