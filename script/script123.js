3bb669a60dba8fba865c1659c248e516c87c0476
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-06 22:19:39 +0300

    Task123

---
+++ script/script123.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <= 9; i++) {
+	for (let j = 1; j <= 3; j++) {
+		document.write(i);
+	}
+}
+// задание 2
+for (let i = 1; i <= 3; i++) {
+    document.write(' ')
+    for (let k = 1; k <= 3; k++) {
+        document.write('\n' + i + k + ' ');
+    };
+};
