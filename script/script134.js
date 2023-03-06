b4775293b5b6a1f2e1ce95469b3c0b5506996cd8
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-06 22:48:14 +0300

    Task134

---
+++ script/script134.js
@@ -0,0 +1,10 @@
+let arr = [10, 20, 30, 40, 21, 32, 51]; 
+let sum = 0;
+for (let elem of arr) {
+    let num = String(arr[i]);
+    let char = num[0];
+	if (char == 1 || char == 2) {
+		sum += elem;
+	}
+}
+console.log(sum);
