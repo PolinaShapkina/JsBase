738064b332abf3e1b66eb47e753550b88599c928
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-06 22:28:50 +0300

    Task127

---
+++ script/script127.js
@@ -0,0 +1,18 @@
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; 
+	i++) { 
+	arr[i] = arr[i] ** 2;
+}
+console.log(arr);
+let arr1 = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr1.length; 
+	i++) { 
+	arr1[i]--;
+}
+console.log(arr1);
+let arr2 = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr2.length; 
+	i++) { 
+	arr2[i] += 10;
+}
+console.log(arr2);
