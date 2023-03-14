d08af419cadbed02a01e5decc44a57ce83b1f2d3
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-14 18:41:56 +0300

    Task157

---
+++ script/script157.js
@@ -0,0 +1,13 @@
+function func(num) {
+	console.log(num ** 3);
+}
+func(3);
+function func1(num1) {
+	if (num1 > 0){
+        console.log('+++')
+    }
+    else{
+        console.log('---')
+    }
+}
+func1(-3);
