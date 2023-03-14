3fb31d84b4b5a88601554087501eb625f00b6371
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-14 18:40:24 +0300

    Task156

---
+++ script/script156.js
@@ -0,0 +1,11 @@
+function name() {
+	console.log('Polina Shapkina');
+}
+function sum(){
+    var sum = 0;
+    for (var i = 0; i <= 100; i++) {
+        sum += i;
+    }
+    console.log(sum);
+}
+sum()
