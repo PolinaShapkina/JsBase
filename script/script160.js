48e97151ebb0b83b9a15688e81348413582a1cdd
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-15 17:23:17 +0300

    Task160

---
+++ script/script160.js
@@ -0,0 +1,12 @@
+function func(num = 5) {
+	console.log(num * num);
+}
+func(2);
+func(3);
+func();
+function func1(num1 = 0, num2 = 0) {
+	console.log(num1 + num2);
+}
+func1(2, 3);
+func1(3);
+func1();
