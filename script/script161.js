186cfa148c222fb7616899a6ec49b1e80f234dfb
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-15 17:25:26 +0300

    Task161

---
+++ script/script161.js
@@ -0,0 +1,10 @@
+function func(num) {
+	return num ** 3;
+}
+let res = func(3);
+console.log(res);
+function func1(num1) {
+	return Math.sqrt(num1);
+}
+let res1 = func1(4) + func1(3);
+console.log(res1);
