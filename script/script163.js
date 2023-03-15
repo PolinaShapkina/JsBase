8b043de89d625f1d2246cd18255f3674efbc4fa6
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-15 17:30:33 +0300

    Task163

---
+++ script/script163.js
@@ -0,0 +1,25 @@
+function func(num) {
+	if (num <= 0) {
+		return Math.abs(num);
+	}
+	
+	return num ** 2;
+}
+console.log( func(10) );
+console.log( func(-5) );
+function func1(num) {
+	return num;
+	
+	let res = num ** 2;
+	return res;
+}
+console.log( func1(3) );
+function func2(num) {
+	if (num <= 0) {
+		return Math.abs(num);
+	} else {
+		return num ** 2;
+	}
+}
+console.log( func2(10) );
+console.log( func2(-5) );
