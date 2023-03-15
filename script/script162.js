ea3553b03efb29889cebbf44ca5d8b9fe1be8f36
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-15 17:28:27 +0300

    Task162

---
+++ script/script162.js
@@ -0,0 +1,21 @@
+function round2(num) {
+    return num.toFixed(3);
+}
+let res2 = round2(sum1(sqrt1(2), sqrt1(3), sqrt1(4)));
+console.log(res2);
+function sqrt(num) {
+	return Math.sqrt(num);
+}
+function round(num) {
+	return num.toFixed(3);
+}
+let res = round(sqrt(2));
+console.log(res)
+function sqrt1(num4) {
+	return Math.sqrt1(num4);
+}
+function sum(num1, num2, num3) {
+	return num1 + num2 + num3;
+}
+let res1 = sum(sqrt1(2), sqrt1(3), sqrt1(4));
+console.log(res1)
