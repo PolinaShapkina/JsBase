f552a15a0e0be97d8657c3428de4fe1f4c8c2718
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-22 18:21:21 +0300

    Task170

---
+++ script/script170.js
@@ -0,0 +1,34 @@
+
+function func11() {
+    return 3;
+}
+function func12() {
+    return 5;
+    }
+console.log(func11() + func12());
+function sum2(arr) {
+    let res = 0;
+for (let elem of arr) {
+    res += elem;
+}
+    return res;
+}
+console.log(sum2([1, 2, 3, 4, 5]));
+let arr = [1, 2, 3, 4, 5];
+function func3(array) {
+    let res = 0;
+for (let elem of array) {
+    res += elem;
+};
+return res;
+};
+console.log(func3(arr))
+let arr5 = [1, 2, 3, 4, 5];
+console.log(sum5(arr5));
+function sum5(arr) {
+    let sum = 0;
+for (let elem of arr) {
+    sum += elem;
+}
+return sum;
+}
