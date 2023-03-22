2fbd5973c3cb2f48f2136a50f5f29012d7926720
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-22 19:27:30 +0300

    Task195

---
+++ script/script195.js
@@ -0,0 +1,14 @@
+let result = every([1, 2, 3, 4, 5], 
+	elem => elem > 0);
+let result1 = every([1, 2, 3, 4, 5], 
+	elem, index => elem * index > 10);
+let arr = [[1, 2, 3], [4, 5, 6], 
+	[7, 8, 9]]; 
+let result2 = each(arr, function(elem, 
+	index) { 
+	if (elem * index > 10) {
+		return true;
+	} else {
+		return false;
+	}
+});
