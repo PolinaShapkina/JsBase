10f28c60d5bad99f8452470e5aee631430297841
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-22 18:48:18 +0300

    Task181

---
+++ script/script181.js
@@ -0,0 +1,21 @@
+function func() {
+	console.log('!');
+}
+let func1 = function() {
+	console.log('!');
+}
+func2(); 
+function func2() {
+	console.log('!!!!!!!');
+}
+func3(); 
+let func3 = function() {
+	console.log('!!!!');
+};
+let func4 = function() {console.log('!')};
+let func5 = function() {
+	console.log('!')
+};
+function func6() {
+	console.log('!')
+}
