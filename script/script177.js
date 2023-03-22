298fb718add06bdeb852b4536118704b29fe8823
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-22 18:37:48 +0300

    Task177

---
+++ script/script177.js
@@ -0,0 +1,18 @@
+function func(num) {
+	num = 2;
+}
+let num = 1;
+func(num);
+console.log(num);
+function func1() {
+	num1 = 2;
+}
+let num1 = 1;
+func1();
+console.log(num1);
+function func2() {
+	let num2 = 2;
+}
+let num2 = 1;
+func2();
+console.log(num2);
