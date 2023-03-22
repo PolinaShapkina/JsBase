e627a7d1402620308219755819e1b8729e155931
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-22 18:50:44 +0300

    Task182

---
+++ script/script182.js
@@ -0,0 +1,26 @@
+function func() {
+	console.log('!');
+}
+let func = function() {
+	console.log('!');
+}
++function() {
+	console.log('!');
+}
+!function func() {
+	console.log('!');
+}
+-function func() {
+	console.log('!');
+}
+1 + function func() {
+	console.log('!');
+}
+(function func() {
+	console.log('!');
+})
+console.log(
+	function() {
+		console.log('!');
+	}
+);
