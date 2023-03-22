f9beecc99d964a4528aa56f7262017290563136d
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-03-22 19:09:26 +0300

    Task187

---
+++ script/script187.js
@@ -0,0 +1,12 @@
+function func(num1, num2) {
+    function square(num) {
+        return num * num;
+    }
+    function cube(num) {
+        return num * num * num; 
+    }
+
+    return square(num1) + cube(num2);
+}
+
+console.log(func(4, 7));
