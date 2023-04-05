1dcdf7ad0c09b4cf0c1e21ab590b4029a78a4b62
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-04-05 19:38:41 +0300

    Task 248

---
+++ script/script248.js
@@ -0,0 +1,9 @@
+let p1 = document.querySelector("#p1");
+let btn1 = document.querySelector("#btn1");
+btn1.addEventListener("click", () => {
+    console.log(p1.innerHTML);
+});
+let btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", () => {
+    p1.innerHTML = `<b>${p1.textContent}</b>`
+});
