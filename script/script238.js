3821e10a10dc0eba310644cbe1bfda9f13c1804e
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-04-05 18:00:24 +0300

    Task 238

---
+++ script/script238.js
@@ -0,0 +1,7 @@
+let date1 = new Date(2020, 1, 1);
+let date2 = new Date(2019, 1, 1);
+console.log(date1 > date2);
+let date3 = new Date(2020, 1, 1);
+let date4 = new Date(2021, 1, 1);
+
+console.log(date3 > date4);
