4a34f667037d74c08cc51375a688af6231360a2d
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-05-08 23:36:35 +0300

    Task 293

---
+++ script/script293.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    let elem = document.getElementById('elem');
+
+elem.addEventListener('click', function(event) {
+if (event.altKey) {
+    elem.style.backgroundColor = 'red';
+}
+});
+// 2
+let elem1 = document.querySelector('#elem');
+elem1.addEventListener('click', function(event1) {
+if (event1.target.tagName === 'LI') {
+    if (event1.ctrlKey) {
+    event1.target.textContent += '1';
+    } else if (event1.shiftKey) {
+    event1.target.textContent += '2';
+    }
+}
+});
+}
