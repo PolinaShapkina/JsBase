58639b7ea71bb9e801543e9c0846e7ed79767c50
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-05-11 22:18:41 +0300

    Task 304

---
+++ script/script304.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    "use strict";
+
+let elem = document.querySelector('#elem');
+elem.addEventListener('blur', func);
+
+function func() {
+	alert( square.call(this) );
+	
+	function square() {
+		return this.value * this.value;
+	}
+}
+}
