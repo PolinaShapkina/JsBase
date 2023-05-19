e8e7395e7717f37674a8f6456c947ef96e39c3d4
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-05-19 23:33:10 +0300

    Task 332

---
+++ script/script332.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    let employees = [
+            {name: 'employee1', age: 30, salary: 400}, 
+            {name: 'employee2', age: 31, salary: 500}, 
+            {name: 'employee3', age: 32, salary: 600}, 
+        ];
+        for (let i = 0; i < employees.length; i++) {
+            let tr = document.createElement('tr');
+            let nameTd = document.createElement('td');
+            nameTd.innerText = employees[i].name;
+            let ageTd = document.createElement('td');
+            ageTd.innerText = employees[i].age;
+            ageTd.classList.add('age'); 
+            let salaryTd = document.createElement('td');
+            salaryTd.innerText = employees[i].salary;
+            tr.appendChild(nameTd);
+            tr.appendChild(ageTd);
+            tr.appendChild(salaryTd);
+            document.querySelector('tbody').appendChild(tr);
+        }
+        
+        let ageTds = document.querySelectorAll('.age');
+        ageTds.forEach(td => {
+            td.addEventListener('click', () => {
+            let age = parseInt(td.innerText);
+            td.innerText = age + 1;
+            });
+        });
+        }
