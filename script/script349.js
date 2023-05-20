42899e740be823a509b6789579b67aa10d52a3da
PolinaShapkina <shapkina.p.1.21@gmail.com>
2023-05-20 23:06:45 +0300

    Task 349

---
+++ script/script349.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    function appendText(selector, text) {
+        let elems = document.querySelectorAll(selector);
+        
+        for (let elem of elems) {
+            elem.textContent += text;
+        }
+    }
+    appendText('.elem', 'text');
+    }
